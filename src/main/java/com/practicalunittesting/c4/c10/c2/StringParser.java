8e32ce0079f31d80e0b5e680c34cad0656efe749
Pavlo Plynko <pavlo.plynko@gmail.com>
2017-10-25 22:54:11 +0300

    4.10.2 fix

--- src/main/java/com/practicalunittesting/c4/c10/c2/StringParser.java
+++ src/main/java/com/practicalunittesting/c4/c10/c2/StringParser.java
@@ -7,13 +7,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringParser {
-    public static List<Integer> getThreeDigitNumbers(String string) {
+    public static List<Integer> getNumbersList(String string) {
         if (string == null || string.length() == 0) {
             return Collections.emptyList();
         }
 
         List<Integer> result = new ArrayList<>();
-        Matcher matcher= Pattern.compile("\\d{3}").matcher(string);
+        Matcher matcher= Pattern.compile("-?\\d{3,}").matcher(string);
         while (matcher.find()) {
             result.add(Integer.parseInt(matcher.group()));
         }
--- src/test/java/com/practicalunittesting/c4/c10/c2/GetNumbersListTest.java
+++ src/test/java/com/practicalunittesting/c4/c10/c2/GetNumbersListTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnitParamsRunner.class)
-public class GetThreeDigitNumbersTest {
+public class GetNumbersListTest {
 
     private static Object[] getParams() {
         return new Object[]{
@@ -20,20 +20,22 @@ public class GetThreeDigitNumbersTest {
                 new Object[]{"", Collections.EMPTY_LIST},
                 new Object[]{"000", Collections.singletonList(0)},
                 new Object[]{"123", Collections.singletonList(123)},
-                new Object[]{"1234", Collections.singletonList(123)},
-                new Object[]{"12345", Collections.singletonList(123)},
-                new Object[]{"12 345", Collections.singletonList(345)},
-                new Object[]{"1 234 5", Collections.singletonList(234)},
-                new Object[]{"123456", Arrays.asList(123, 456)},
-                new Object[]{"111222333444555666777888", Arrays.asList(111, 222, 333, 444, 555, 666, 777, 888)},
+                new Object[]{"-12 345", Collections.singletonList(345)},
+                new Object[]{"1-234 5", Collections.singletonList(-234)},
+                new Object[]{"123456", Collections.singletonList(123456)},
+                new Object[]{"2147483647", Collections.singletonList(2147483647)},
+                new Object[]{"-2147483648", Collections.singletonList(-2147483648)},
                 new Object[]{"asdfa123sdafa sadfasdf456 789sdfsdf", Arrays.asList(123, 456, 789)},
                 new Object[]{"12.34_56e78+90", Collections.EMPTY_LIST},
+                new Object[]{"abc 12", Collections.EMPTY_LIST},
+                new Object[]{"cdefg 345 12bb23", Collections.singletonList(345)},
+                new Object[]{"cdefg 345 12bbb33 678tt", Arrays.asList(345, 678)},
         };
     }
 
     @Test
     @Parameters(method = "getParams")
     public void getValidList(String inputString, List<Integer> expectedList) {
-        assertEquals(expectedList, StringParser.getThreeDigitNumbers(inputString));
+        assertEquals(expectedList, StringParser.getNumbersList(inputString));
     }
 }
